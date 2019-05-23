e68a24ff871219aa1bd9df08eb16a20d2d2dfe0b
Gabriel <themrhetch@outlook.com>
2019-05-23 09:58:39 -0300

    Close implementado

--- Projeto Jadir 2019-1/src/br/edu/unicid/util/ConnectionFactory.java
+++ Projeto Jadir 2019-1/src/br/edu/unicid/util/ConnectionFactory.java
@@ -6,11 +6,39 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 	public static Connection getConnection() throws SQLException {
+		Class.forName("");
+		
 		String url = "jdbc:mysql://localhost:3306";
 		String usr = "admin";
 		String pass = "";
 		
 		// Retorna a conexao com MYSQL de acordo com url, usuario e sua senha
 		return DriverManager.getConnection(url, usr, pass);
+	}
+	
+	public static void close (Connection conn, PreparedStatement ps, ResultSet rs) 
+		close(conn, ps);
+		try {
+			rs.close();
+		} catch (SQLException e) {
+			
+		}
+	}
+	
+	public static void close (Connection conn, PreparedStatement ps) {
+		close(conn);
+		try {
+			ps.close();
+		} catch (SQLException e) {
+			
+		}
+	}
+	
+	public static void close (Connection conn) {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			
+		}
 	}
 }
