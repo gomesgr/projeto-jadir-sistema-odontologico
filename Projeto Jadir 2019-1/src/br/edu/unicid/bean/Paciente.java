d7e616766a2ad1172dd871b56e4eddc26c23884d
Rodrigo <prehistoriclife65@gmail.com>
2019-05-23 11:55:22 -0300

    Add files via upload

--- Projeto Jadir 2019-1/src/br/edu/unicid/bean/Paciente.java
+++ Projeto Jadir 2019-1/src/br/edu/unicid/bean/Paciente.java
@@ -1,6 +1,52 @@
 package br.edu.unicid.bean;
 
 // TODO implementar dados necessarios para a construcao do programa
-public class Paciente {
+public class Paciente 
+{
+	private Integer cpf;
+	private String nome;
+	private String login;
+	private String senha;
+	
+	public Paciente(Integer cpf, String nome, String login, String senha)
+	{
+		this.cpf = cpf;
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+	}
 
+	public Integer getCpfPaciente() {
+		return cpf;
+	}
+
+	public void setCpfPaciente(Integer cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNomePaciente() {
+		return nome;
+	}
+
+	public void setNomePaciente(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLoginPaciente() {
+		return login;
+	}
+
+	public void setLoginPaciente(String login) {
+		this.login = login;
+	}
+
+	public String getSenhaPaciente() {
+		return senha;
+	}
+
+	public void setSenhaPaciente(String senha) {
+		this.senha = senha;
+	}
+	
+	
 }
