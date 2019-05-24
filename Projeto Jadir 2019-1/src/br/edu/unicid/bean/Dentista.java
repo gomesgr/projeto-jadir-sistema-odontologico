4bbd22465ff54ce9b5b7fdad4fcd0db96fbe30a8
Rodrigo <prehistoriclife65@gmail.com>
2019-05-24 13:48:20 -0300

    Add files via upload

---
+++ Projeto Jadir 2019-1/src/br/edu/unicid/bean/Dentista.java
@@ -0,0 +1,50 @@
+package br.edu.unicid.bean;
+
+// TODO implementar dados necessarios para a construcao do programa
+public class Dentista 
+{
+	private Integer id;
+	private String nome;
+	private Integer cro;
+	private String telefone;
+	
+	public Dentista(Integer id, String nome, Integer cro, String telefone)
+	{
+		this.id = id;
+		this.nome = nome;
+		this.cro = cro;
+		this.telefone = telefone;
+	}
+	
+	public Integer getCroDentista() {
+		return cro;
+	}
+
+	public void setCroDentista(Integer cro) {
+		this.cro = cro;
+	}
+
+	public String getTelefoneDentista() {
+		return telefone;
+	}
+
+	public void setTelefoneDentista(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Integer getIdDentista() {
+		return id;
+	}
+
+	public void setIdDentista(Integer cpf) {
+		this.id = cpf;
+	}
+
+	public String getNomeDentista() {
+		return nome;
+	}
+
+	public void setNomeDentista(String nome) {
+		this.nome = nome;
+	}
+}
